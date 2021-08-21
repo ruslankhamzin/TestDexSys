f755ada2da9b94e749caedd83b66e2d5eb60e0c4
Ruslan Khamzin <hamzin362@gmail.com>
2021-08-21 18:47:17 +0400

    First Step

---
+++ src/main/java/org/example/Commands.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Commands {
+    public static String[] commands ={"add","print","remove","clear","help"};
+}
---
+++ src/main/java/org/example/Reader.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class Reader
+{
+
+    public static void main( String[] args )
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            try {
+                String[] input = reader.readLine().split(" ");
+                switch (input.)
+            }
+            catch (Exception e){
+                System.out.println(" ");
+
+            }
+        }
+    }
+}
